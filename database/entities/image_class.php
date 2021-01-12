c1d4bb77a7274357bae1169625c6240956a05dff
plasticdino <kieefuanh@gmail.com>
2021-01-12 22:47:46 +0700

    admin-product add DONE-FE BE

--- database/entities/image_class.php
+++ database/entities/image_class.php
@@ -5,16 +5,30 @@ class Image{
   public $id;
   public $link;
   public $proid;
-}
 
-public function __construct ($id, $link, $proid)
+public function __construct ($link, $proid)
 {
-  $this->id = $id;
   $this->link = $link;
   $this->proid = $proid;
 }
 
 public function insert_image(){
-  
+
+  $db = new Db();
+  $sql = "INSERT INTO Image (ImageLink, ProductId) VALUES
+  ('$this->link', '$this->proid')";
+
+  $result = $db->query_execute($sql);
+  return $result;
+}
+
+// delete imagearc
+public function delete_image($id)
+{
+  $db = new Db();
+  $sql = "DELETE from Image where ImageId = '$id'" ;
+  $result =  $db->query_execute($sql);
+  return $result;
+}
 }
  ?>
--- pages/admin/product_add.php
+++ pages/admin/product_add.php
@@ -4,11 +4,13 @@
   include_once("partials/header.php");
   require_once("../../database/entities/product_class.php");
   require_once("../../database/entities/category_class.php");
+  require_once("../../database/entities/image_class.php");
 
   if(isset($_POST["btnsubmit"])){
     $proId = $_POST["txtProId"];
     $proName = $_POST["txtProName"];
     $proPrice = $_POST["txtProPrice"];
+    $proSale = $_POST["txtProSale"];
     $proDesc = $_POST["txtProDesc"];
     $proStorage = $_POST["txtProStorage"];
     $cateId = $_POST["txtCateID"];
@@ -16,14 +18,42 @@
 
     $newProduct = new Product ($proId, $proName, $proPrice, $proDesc, $proStorage,$cateId, $proImage,'');
 
-    $result = $newProduct->insert_product();
-    if(!$result)
+    $id_exist = $newProduct->id_exist();
+    if (!$id_exist)
     {
-      header("Location: product_add.php?failure");
+      header("Location: product_add.php?exist");
     }
     else
     {
-      header("Location: product_add.php?inserted");
+      $result = $newProduct->insert_product();
+
+      if(!empty(array_filter($_FILES['files']['name']))) {
+          // Loop through each file in files[] array
+          foreach ($_FILES['files']['tmp_name'] as $key => $value)
+          {
+            $file_tmpname = $_FILES['files']['tmp_name'][$key];
+            $file_name = $_FILES['files']['name'][$key];
+            // Set upload file path
+            $timestamp = date("Y").date("m").date("d").date("h").date("i").date("s");
+            $filepath = "../../media/image/shop/".$timestamp.$file_name;
+            if( move_uploaded_file($file_tmpname, $filepath)) {
+                $newImage = new Image($filepath,$proId);
+                $result = $newImage->insert_image();
+            }
+            else {
+                echo "Error uploading {$file_name} <br />";
+            }
+          }
+      }
+      ///notification
+      if(!$result)
+      {
+        header("Location: product_add.php?failure");
+      }
+      else
+      {
+        header("Location: product_add.php?inserted");
+      }
     }
   }
  ?><body>
@@ -39,12 +69,20 @@
                     $notification = "Insert product successfully !!!";
                     include_once("partials/notify.php");
                   }
+                  else if(isset($_GET["exist"])){
+                    $notification = "Product Id exists !!!";
+                    include_once("partials/notify.php");
+                  }
+                  if(isset($_GET["failure"])){
+                    $notification = "Insert product fail !!!";
+                    include_once("partials/notify.php");
+                  }
                  ?><h4 class="card-title">Add product</h4>
                 <form class="forms-sample" method="post" enctype="multipart/form-data">
 
                   <div class="form-group">
                     <label for="ProductName">Product Id</label>
-                      <input type="text" class="form-control" id="ProductId"
+                      <input type="text" class="form-control" required="true"
                       name="txtProId" placeholder="Product Id"
                       value="<?php echo isset($_POST["txtProId"])?$_POST["txtProId"]:""; ?>"/>
                   </div>
@@ -66,40 +104,55 @@
 
                   <div class="form-group">
                     <label for="ProductName">Product Name</label>
-                    <input type="text" class="form-control" id="ProductName"
+                    <input type="text" class="form-control" required="true"
                     name="txtProName" placeholder="Product Name"
                     value="<?php echo isset($_POST["txtProName"])?$_POST["txtProName"]:""; ?>"/>
                   </div>
 
                   <div class="form-group">
                     <label for="ProductPrice">Product Price</label>
-                    <input type="number" class="form-control" id="ProductPrice"
+                    <input type="number" class="form-control" required ="true"
                     name="txtProPrice" placeholder="Product Price"
                     value="<?php echo isset($_POST["txtProPrice"])?$_POST["txtProPrice"]:""; ?>"/>
                   </div>
 
                   <div class="form-group">
+                    <label for="ProductPrice">Product Sale Price</label>
+                    <input type="number" class="form-control"
+                    name="txtProSale" placeholder="Product Sale Price"
+                    value="<?php echo isset($_POST["txtProSale"])?$_POST["txtProSale"]:""; ?>"/>
+                  </div>
+
+                  <div class="form-group">
                     <label for="ProductDescription">Product Description</label>
-                    <input type="text" class="form-control" id="ProductDescription"
+                    <input type="text" class="form-control" required ="true"
                     name="txtProDesc" placeholder="Product Description"
                     value="<?php echo isset($_POST["txtProDesc"])?$_POST["txtProDesc"]:""; ?>"/>
                   </div>
 
                   <div class="form-group">
                     <label for="ProductStorage">Product Storage</label>
-                    <input type="number" class="form-control" id="ProductStorage"
+                    <input type="number" class="form-control" required ="true"
                     name="txtProStorage" placeholder="Product Storage"
                     value="<?php echo isset($_POST["txtProStorage"])?$_POST["txtProStorage"]:""; ?>"/>
                   </div>
 
+                  <!-- product cove image in product table column image -->
                   <div class="form-group">
-                    <label for="ProductImage">Product Image</label>
-                    <input type="file" class="form-control" id="ProductImage" name="txtProImage"
+                    <label for="ProductImage">Product Cover Image</label>
+                    <input type="file" class="form-control" name="txtProImage" required ="true"
                     accept=".PNG, .GIF, .JPG">
                   </div>
 
+                  <!-- images -->
+                  <div class="form-group">
+                    <label for="ProductImage">Product Image</label>
+                    <input type="file" class="form-control" name="files[]"
+                    accept=".PNG, .GIF, .JPG" multiple>
+                  </div>
+
                   <button type="submit" class="btn btn-primary mr-2" name="btnsubmit">SAVE</button>
-                  <button class="btn btn-light">CANCEL</button>
+                  <button class="btn btn-light"onclick="location.href='index.php'">CANCEL</button>
                 </form>
               </div>
             </div><?php
